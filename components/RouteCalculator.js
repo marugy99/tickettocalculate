df83a317629dc96bb9e91dced5167011525f0faa
marugy99 <marugy99@gmail.com>
2022-03-14 23:00:11 -0300

    New styling and route length functionality

--- components/RouteCalculator.js
+++ components/RouteCalculator.js
@@ -1,5 +1,6 @@
 const RouteCalculator = ({
   routePoints,
+  routeLength,
   handleRoutePoints,
   resetRoutePoints,
 }) => {
@@ -40,18 +41,24 @@ const RouteCalculator = ({
 
   return (
     <>
-      <div className="mt-4 grid grid-cols-2 gap-2">
+      <div className="mt-8 grid grid-cols-2 gap-2 md:grid-cols-4">
         {pointButtons.map((btn) => (
-          <div className="grid grid-cols-2 gap-2 text-center" key={btn.point}>
-            <p className="text-lg">
-              {btn.point}
-              <span className="block text-sm text-gray-500">
-                {btn.multiplyBy} points
-              </span>
-            </p>
-            <div className="flex gap-1">
+          <section
+            className="grid-rows-[1fr, 40px] grid items-center justify-center gap-4 rounded bg-slate-700 py-6 px-4 text-center text-white"
+            key={btn.point}
+          >
+            <div>
+              <p className="text-2xl font-bold md:text-3xl">{btn.point}</p>
+              <img
+                className="mx-auto h-12 w-12 md:h-16 md:w-16"
+                src="./wagon.svg"
+                alt="Train ticket icon"
+                aria-hidden="true"
+              />
+            </div>
+            <div>
               <button
-                className="rounded bg-white py-2 px-4 hover:bg-gray-100"
+                className="mr-1 inline-block rounded bg-green-100 py-2 px-4 text-sm font-semibold text-green-800 hover:bg-green-200 md:text-base"
                 value={btn.point}
                 data-multiply={btn.multiplyBy}
                 data-action="add"
@@ -60,7 +67,7 @@ const RouteCalculator = ({
                 +
               </button>
               <button
-                className="rounded bg-white py-2 px-4 hover:bg-gray-100"
+                className="inline-block rounded bg-rose-100 py-2 px-4 text-sm font-semibold text-rose-800 hover:bg-rose-200 md:text-base"
                 value={btn.point}
                 data-multiply={btn.multiplyBy}
                 data-action="deduct"
@@ -68,17 +75,25 @@ const RouteCalculator = ({
               >
                 -
               </button>
+              <span className="mt-2 block text-sm font-normal italic text-slate-400">
+                {btn.multiplyBy} point{btn.point > 1 ? "s" : ""}
+              </span>
             </div>
-          </div>
+          </section>
         ))}
       </div>
-      <p className="my-4 text-lg">Total: {routePoints}</p>
-      <button
-        className="mt-4 mr-2 rounded bg-red-600 py-2 px-4 text-white"
-        onClick={resetRoutePoints}
-      >
-        Reset
-      </button>
+      <section className="mt-6 flex flex-wrap items-center justify-between gap-4">
+        <div className="text-xl font-semibold text-white md:text-2xl">
+          <p className="my-4">Total points: {routePoints}</p>
+          <p>Total length: {routeLength}</p>
+        </div>
+        <button
+          className="mt-4 mr-2 rounded bg-rose-600 py-2 px-4 font-medium text-white hover:bg-rose-700"
+          onClick={resetRoutePoints}
+        >
+          Reset
+        </button>
+      </section>
     </>
   );
 };
