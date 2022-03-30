707f154b08eae421c999914d30a6aa6faf8fa212
marugy99 <marugy99@gmail.com>
2022-03-29 21:52:54 -0300

    Adjust existing name rule

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,7 @@ const Home = () => {
   const addPlayer = (e) => {
     e.preventDefault();
 
-    const nameExists = players.some(
-      (player) => player.playerName == name
-    );
+    const nameExists = players.some((player) => player.playerName == name);
 
     if (name && !nameExists) {
       setPlayers([
@@ -23,7 +21,7 @@ const Home = () => {
 
       setName("");
     } else if (nameExists) {
-      setName("exists")
+      setName(false);
     }
   };
 
@@ -39,10 +37,13 @@ const Home = () => {
     <main className="isolate min-h-screen w-full bg-slate-900">
       <Head>
         <title>Ticket to Ride Calculator</title>
-        <meta name="description" content="Score calculator for the board game Ticket to Ride: Europe" />
+        <meta
+          name="description"
+          content="Score calculator for the board game Ticket to Ride: Europe"
+        />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <header className="container mx-auto pt-10 text-center px-4 sm:px-0">
+      <header className="container mx-auto px-4 pt-10 text-center sm:px-0">
         <img
           className="mx-auto h-20 w-20"
           src="/train-ticket.svg"
@@ -50,31 +51,47 @@ const Home = () => {
           aria-hidden="true"
         />
         <div>
-          <h1 className="mt-4 text-4xl sm:text-5xl font-bold text-white">
+          <h1 className="mt-4 text-4xl font-bold text-white sm:text-5xl">
             Ticket to Ride Calculator
           </h1>
-          <p className="text-white text-sm text-center mt-4">
-            Made with ♡ by <a href="https://marucodes.com/" rel="noreferrer" target="_blank" className="hover:underline inline-block">Maru</a> 
+          <p className="mt-4 text-center text-sm text-white">
+            Made with ♡ by{" "}
+            <a
+              href="https://marucodes.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="inline-block hover:underline"
+            >
+              Maru
+            </a>
           </p>
         </div>
-        <form action="#" onSubmit={(e) => addPlayer(e)} className="flex items-start mt-4 justify-center gap-2 flex-wrap">
+        <form
+          action="#"
+          onSubmit={(e) => addPlayer(e)}
+          className="mt-4 flex flex-wrap items-start justify-center gap-2"
+        >
           <div>
             <input
               type="text"
-              className="rounded bg-gray-100 py-2 px-4 w-full sm:w-auto"
+              className="w-full rounded bg-gray-100 py-2 px-4 sm:w-auto"
               placeholder="Enter name"
-              value={name === "exists" ? "" : name}
+              value={name === false ? "" : name}
               onChange={(e) => setName(e.target.value)}
               required
             />
-            {name === "exists" && <p className="text-rose-400 text-sm mt-4">This name already exists. Enter a different one.</p>}
+            {name === false && (
+              <p className="mt-4 text-sm text-rose-400">
+                This name already exists. Enter a different one.
+              </p>
+            )}
           </div>
           <button className="rounded bg-gradient-to-r from-indigo-500 to-sky-500 py-2 px-4 font-semibold text-white hover:from-indigo-700">
             Add new player
           </button>
         </form>
       </header>
-      <div className="container mx-auto mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 min-h-fit lg:gap-4 pb-4">
+      <div className="container mx-auto mt-8 grid min-h-fit grid-cols-1 gap-4 pb-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-4">
         {players.map((player, index) => (
           <PlayerCard
             name={player.playerName}
